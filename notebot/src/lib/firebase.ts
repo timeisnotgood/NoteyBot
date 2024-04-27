27911ec1a8b233e4d521e97afc3260273a25dd7e
bhoopathi <bhoopathi.avt@gmail.com>
2024-04-27 14:36:24 +0530

    Final Push

---
+++ notebot/src/lib/firebase.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "noteybot-5dfc6.firebaseapp.com",
+  projectId: "noteybot-5dfc6",
+  storageBucket: "noteybot-5dfc6.appspot.com",
+  messagingSenderId: "974004651957",
+  appId: "1:974004651957:web:3b9340dec7a73103a5bbfc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
